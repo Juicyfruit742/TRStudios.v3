7b744aa7f75bbb23db1071d06252ad40d2486494
Jonathan Lyons <jonathanlyons@Jonathans-MacBook-Pro.local>
2017-04-06 00:32:19 -0400

    first commit

---
+++ js/my_java.js
@@ -0,0 +1,57 @@
+// handle links with @href started with '#' only
+$(document).on('click', 'a[href^="#"]', function(e) {
+    // target element id
+    var id = $(this).attr('href');
+
+    // target element
+    var $id = $(id);
+    if ($id.length === 0) {
+        return;
+    }
+
+    // prevent standard hash navigation (avoid blinking in IE)
+    e.preventDefault();
+
+    // top position relative to the document
+    var pos = $id.offset().top;
+
+    // animated top scrolling
+    $('body, html').animate({scrollTop: pos});
+});
+
+// button focus fix
+
+$(".btn").mouseup(function(){
+    $(this).blur();
+})
+
+// affix browser sizing
+
+var $attribute = $('[data-smart-affix]');
+$attribute.each(function(){
+  $(this).affix({
+    offset: {
+      top: $(this).offset().top,
+    }
+  })
+})
+$(window).on("resize", function(){
+  $attribute.each(function(){
+    $(this).data('bs.affix').options.offset = $(this).offset().top
+  })
+})
+
+
+// Morphext Animations
+$("#js-rotating").Morphext({
+    // The [in] animation type. Refer to Animate.css for a list of available animations.
+    animation: "flipInX",
+    // An array of phrases to rotate are created based on this separator. Change it if you wish to separate the phrases differently (e.g. So Simple | Very Doge | Much Wow | Such Cool).
+    separator: ",",
+    // The delay between the changing of each phrase in milliseconds.
+    speed: 2000,
+    complete: function () {
+        // Called after the entrance animation is executed.
+    }
+});
+
