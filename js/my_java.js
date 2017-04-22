c2f104857e7754958ec4cf2f0823e4cada531594
Jonathan Lyons <jonathanlyons@jonathans-mbp.attlocal.net>
2017-04-21 20:51:19 -0400

    update

--- js/my_java.js
+++ js/my_java.js
@@ -1,22 +1,17 @@
-// handle links with @href started with '#' only
-$(document).on('click', 'a[href^="#"]', function(e) {
-    // target element id
-    var id = $(this).attr('href');
-
-    // target element
-    var $id = $(id);
-    if ($id.length === 0) {
-        return;
+// slowdown page scrolling links with @href started with '#' only
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
     }
-
-    // prevent standard hash navigation (avoid blinking in IE)
-    e.preventDefault();
-
-    // top position relative to the document
-    var pos = $id.offset().top;
-
-    // animated top scrolling
-    $('body, html').animate({scrollTop: pos});
+  });
 });
 
 // button focus fix
@@ -53,3 +48,4 @@ $('.parent-container').magnificPopup({
   type: 'image'
   // other options
 });
+
